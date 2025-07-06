1ad8f92291cd1aed401b448c229bf0a5cdd354ce
digvijay778 <160209022+digvijay778@users.noreply.github.com>
2025-07-06 19:31:02 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js';
 import ApiError from '../utils/ApiError.js';
-import ApiResponse from '../utils/apiResponse.js';
+import ApiResponse from '../utils/ApiResponse.js';
 import asyncHandler from '../utils/asynchandler.js';
 import jwt from 'jsonwebtoken'; // Added missing import
 
