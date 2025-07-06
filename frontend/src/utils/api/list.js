9cf2f0b99ef68178ea1fd78fb96cf7435af834c8
digvijay778 <160209022+digvijay778@users.noreply.github.com>
2025-07-07 00:03:12 +0530

    Update list.js

--- frontend/src/utils/api/list.js
+++ frontend/src/utils/api/list.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8001/api/v1" : "/api/v1";
+const BASE_URL = "https://task-trackr-bga4.onrender.com/api/v1";
 // const API_BASE_URL = 'http://localhost:8001/api/v1';
 
 const api = axios.create({
