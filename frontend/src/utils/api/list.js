4cbe5ef88c0303a9f8a150b4304e0ccd823cfff7
digvijay778 <160209022+digvijay778@users.noreply.github.com>
2025-07-06 23:03:07 +0530

    Update list.js

--- frontend/src/utils/api/list.js
+++ frontend/src/utils/api/list.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8001/api/v1" : "api/v1";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8001/api/v1" : "/api/v1";
 // const API_BASE_URL = 'http://localhost:8001/api/v1';
 
 const api = axios.create({
