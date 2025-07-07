e55749347fcd5118fb054a61326d2c06b79e00ae
digvijay778 <160209022+digvijay778@users.noreply.github.com>
2025-07-07 15:47:13 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.MODE_ENV === "development" ? "http://localhost:5000/api" : "/api";
+const BASE_URL = import.meta.env.MODE_ENV === "development" ? "http://localhost:5000/api/v1" : "/api/v1";
 
 // Create axios instance for auth requests
 const authApi = axios.create({
