3db10b6952643a16c7d38527e375c11b642032d2
digvijay778 <160209022+digvijay778@users.noreply.github.com>
2025-07-07 15:47:53 +0530

    Update list.js

--- frontend/src/utils/api/list.js
+++ frontend/src/utils/api/list.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = import.meta.env.MODE_ENV === "development" ? "http://localhost:5000/api" : "/api";
+const BASE_URL = import.meta.env.MODE_ENV === "development" ? "http://localhost:5000/api/v1" : "/api/v1";
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
